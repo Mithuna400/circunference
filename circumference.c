84be307062d52bb1ff1bdb64f48bd9ba8ce962e4
Mithuna400 <poongodimithuna137@gmail.com>
2024-10-20 13:01:07 +0530

    circumference

---
+++ circumference.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+
+{
+    
+    float w;
+    float h;
+    float bmi;
+    printf("enter the weight");
+    scanf("%f",&w);
+    printf("enter the height");
+    scanf("%f",&h);
+    bmi= w/h*h;
+    printf("\n body mass index : %f",bmi);
+}
+
+
+
+
+
+
+
+
